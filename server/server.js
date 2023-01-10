233e5c07fb8397698a98f2614999f78edb7ec2a7
Rahul4683 <rahul.1083838@ratnamcollege.edu.in>
2023-01-10 19:33:08 +0530

    second commit

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
+const openai = new OpenAIApi(configuration);
+
+
 const app = express();
 app.use(cors());
 app.use(express.json());
